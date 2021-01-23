f483eb5b48593fe41d3b9f6f7bb16ce32f561f38
Canoso <jpcanoso@hotmail.com>
2021-01-23 23:32:07 +0000

    test with url

--- src/wait-for-rabbit.sh
+++ src/wait-for-rabbit.sh
@@ -4,7 +4,7 @@ set -e
 
 cmd="$@"
   
-until curl -i -s -f -o /dev/null -u ${USER}:${PASS} http://${HOST}:${MNG_PORT}/api/whoami; do
+until curl -i -s -f -o /dev/null -u ${USER}:${PASS} ${URL}; do
   >&2 echo "RabbitMQ is not ready - waiting ${TIME}s"
   sleep ${TIME}
 done
