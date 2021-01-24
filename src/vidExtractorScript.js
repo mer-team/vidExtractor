e9d9e9042285f1d4ec65b8e1ebe7f0f2874ef7c6
Canoso <jpcanoso@hotmail.com>
2021-01-24 04:31:35 +0000

    Added debug

--- src/vidExtractorScript.js
+++ src/vidExtractorScript.js
@@ -64,6 +64,7 @@ extractVideo = async (url,ch) => {
  */
 startScript = async () => {
     console.log("Starting")
+    console.log('amqp://${user}:${pass}@${host}/')
     amqp.connect('amqp://${user}:${pass}@${host}/', function(err, conn) {
     conn.createChannel(function(err, ch) {
         console.log("Connected")
